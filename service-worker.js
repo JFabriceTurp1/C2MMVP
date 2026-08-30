a0fe298dab85999381c19b0e9cfa19eb970a5408
JFabriceTurp1 <khmg8np9yj@privaterelay.appleid.com>
2026-08-30 08:37:54 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,8 @@
-const CACHE_NAME = "competences-mv-v88-1-pwa-syncfix-4";
-const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
+const CACHE_NAME = "competences-mv-v88-2-pwa-syncstable-5";
+const APP_SHELL = ["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
 });
 
 self.addEventListener("activate", event => {
@@ -23,35 +13,31 @@ self.addEventListener("activate", event => {
   );
 });
 
-function isFirebaseOrGoogle(url) {
-  return (
-    url.hostname.includes("firebase") ||
-    url.hostname.includes("googleapis.com") ||
-    url.hostname.includes("gstatic.com") ||
-    url.hostname.includes("google.com")
-  );
+function bypassNetworkCache(url){
+  return url.hostname.includes("firebase") ||
+         url.hostname.includes("googleapis.com") ||
+         url.hostname.includes("gstatic.com") ||
+         url.hostname.includes("google.com");
 }
 
 self.addEventListener("fetch", event => {
   const req = event.request;
   const url = new URL(req.url);
-  if (req.method !== "GET") return;
-  if (isFirebaseOrGoogle(url)) return;
+  if(req.method !== "GET") return;
+  if(bypassNetworkCache(url)) return;
 
-  if (req.mode === "navigate") {
+  if(req.mode === "navigate"){
     event.respondWith(
-      fetch(req)
-        .then(res => {
-          const copy = res.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put("./index.html", copy));
-          return res;
-        })
-        .catch(() => caches.match("./index.html"))
+      fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put("./index.html", copy));
+        return res;
+      }).catch(() => caches.match("./index.html"))
     );
     return;
   }
 
-  if (url.origin === self.location.origin) {
+  if(url.origin === self.location.origin){
     event.respondWith(
       caches.match(req).then(cached => cached || fetch(req).then(res => {
         const copy = res.clone();
