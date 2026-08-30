38b00ec7f99d1a7a4af18bd6afeb2cc60734b6d0
JFabriceTurp1 <khmg8np9yj@privaterelay.appleid.com>
2026-08-30 08:30:08 +0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,63 @@
+const CACHE_NAME = "competences-mv-v88-1-pwa-syncfix-4";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+function isFirebaseOrGoogle(url) {
+  return (
+    url.hostname.includes("firebase") ||
+    url.hostname.includes("googleapis.com") ||
+    url.hostname.includes("gstatic.com") ||
+    url.hostname.includes("google.com")
+  );
+}
+
+self.addEventListener("fetch", event => {
+  const req = event.request;
+  const url = new URL(req.url);
+  if (req.method !== "GET") return;
+  if (isFirebaseOrGoogle(url)) return;
+
+  if (req.mode === "navigate") {
+    event.respondWith(
+      fetch(req)
+        .then(res => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put("./index.html", copy));
+          return res;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+
+  if (url.origin === self.location.origin) {
+    event.respondWith(
+      caches.match(req).then(cached => cached || fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+        return res;
+      }))
+    );
+  }
+});
