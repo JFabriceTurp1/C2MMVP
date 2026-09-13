8b6f63f2cdafac67b752c965f9485e5f598958c1
JFabriceTurp1 <khmg8np9yj@privaterelay.appleid.com>
2026-09-13 18:15:46 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="competences-mv-v6-pack-complet-manuel";
+const CACHE_NAME="competences-mv-v8-bargraphes-competences";
 const APP_SHELL=["./","./index.html","./manifest.webmanifest","./logo-francois-de-mahy.png","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
