d3fd3e8fb42ed441b7d839ceb718c4e68aae8de3
JFabriceTurp1 <khmg8np9yj@privaterelay.appleid.com>
2026-09-16 14:46:26 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="evaluation-v17-bilans-pdf-fidele";
+const CACHE_NAME="evaluation-v19-historique-enregistre";
 const APP_SHELL=["./","./index.html","./manifest.webmanifest","./logo-francois-de-mahy.png","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
