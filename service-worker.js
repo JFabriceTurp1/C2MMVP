afafe2bea05bab384936e5780249485769294cd3
JFabriceTurp1 <khmg8np9yj@privaterelay.appleid.com>
2026-09-24 19:20:28 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'eval-V107-recherche-synthese-tp';
+const CACHE_NAME = 'eval-V109-portail-correcteur';
 const APP_SHELL=["./index.html","./manifest.webmanifest","./logo-francois-de-mahy.png","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
